ff0d9aa8663bbc741cc0cb2fa48dde6e39546b86
galina <gpischalo@belta.by>
2020-10-02 00:29:34 +0300

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+  if(sampleActivity === undefined || typeof(sampleActivity)!== 'string'  
+      || sampleActivity.length<1  || sampleActivity==='0' ||  !/^[0-9]+$/.test(sampleActivity)){
+    return false;
+  }
+ const year = Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))*HALF_LIFE_PERIOD/0.693);
+ return year>0 ? year : false;
+  
 };
