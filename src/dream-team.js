ade992f8cd96d74fc8e23fe3a0aa71fc321f5681
halinaPP <galunikipp@gmail.com>
2020-10-11 03:42:33 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  if(!members || members.length<1 || !Array.isArray(members)){
+    return false;
+  }
+  const firstLetters = [];
+  members.forEach(item => {
+    if(typeof(item) === "string"){
+      const name = item.trim();
+      if(name.length<1){
+        return false;
+      }
+       firstLetters.push(name[0].toUpperCase());
+    }
+  });
+  firstLetters.sort();
+  return firstLetters.join('');
 };
