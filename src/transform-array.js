c72dd1339344b8998b46e23cc952c5b7ffbe606f
halinaPP <galunikipp@gmail.com>
2020-10-13 01:23:25 +0300

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  if(!Array.isArray(arr)){
+    throw new Error('not array');
+  }
+
+//console.log('arr=',arr);
+let discardNext = false;
+let undefinedElement=false;
+const transformArr = [];
+   arr.map((item,index) => {
+      if(discardNext){
+        discardNext=false;
+        undefinedElement = true;
+        return;
+      }
+     switch (item){
+        case '--discard-next':
+          if(index+1 < arr.length){
+            discardNext = true;
+          }
+          break;
+        case '--discard-prev':
+          if(transformArr.length>1 && !undefinedElement){
+            transformArr.pop();
+          }
+          break;
+        case '--double-next':
+          if(index+1 < arr.length){
+            transformArr.push(arr[index+1]);
+          }
+          break;
+        case '--double-prev':
+          if(transformArr.length>1 && !undefinedElement){
+            transformArr.push(arr[index-1]);
+          }
+          break;
+        default:
+          transformArr.push(item);
+      }
+      if(!discardNext){
+        undefinedElement = false;
+      }
+      return;
+  });
+
+
+  return transformArr;
 };
